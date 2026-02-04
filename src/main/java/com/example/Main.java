5e5776d096c8a2470c8561f80ab6eef910fb742c
bonala-shravani <shravanibonala03@gmail.com>
2026-02-04 12:41:20 +0530

    second commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,15 +1,13 @@
 package com.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++)
+        for (int i = 1; i <= 10; i++)
         {
-            System.out.println("sairam..");
+            System.out.println("Hi "+i);
         }
     }
 }
